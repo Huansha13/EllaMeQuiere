437fb04cc80ada137db5f25ee564c60bbb666a4c
Huansha13 <ing.yfhl13@outlook.com>
2021-04-20 13:45:30 -0500

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ellaMeAma';
+
+  constructor( public dialog: MatDialog) {}
+
+  ngOnInit(): void { }
+
+
+  openDialg(): void {
+    this.dialog.open(DialogComponent);
+  }
+
+ btnMueve(){
+  let width = window.innerWidth;
+  let height = window.innerHeight;
+  let newWidth = (Math.random() * width);
+  let newHeiht = (Math.random() * height);
+
+  document.getElementById('btnSi').style.position = 'absolute'
+  document.getElementById('btnSi').style.left = newWidth + 'px';
+  document.getElementById('btnSi').style.top = newHeiht + 'px';
+ }
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DialogComponent } from './dialog/dialog.component';
+import { MatModule } from './mat/mat.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatModule
   ],
   providers: [],
   bootstrap: [AppComponent]
